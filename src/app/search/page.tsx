80e70f1a416ce3e7e4ea5a4256af9605a80220cb
kawajiharuka <haruka.kawaji@blueish.co.jp>
2024-08-17 12:38:07 +0900

    fix：コンポーネントディレクトリ移動

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,7 +3,7 @@ import { chatFormAction } from "@/lib/actions";
 import { getServerAuthSession } from "@/server/auth";
 import { api, HydrateClient } from "@/trpc/server";
 export default async function Page() {
-  const hello = await api.chat.hello({ text: "chat" });
+  const hello = await api.chat.hello({ text: "search" });
   const messages = await api.chat.getAllMessage();
 
   const session = await getServerAuthSession();
@@ -12,6 +12,8 @@ export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#e3d4f8] to-[#d3d5f7]">
       <p>{hello ? hello.greeting : "Loading tRPC query..."}</p>
+
+      <input type="text" />
       {messages.map((message) => (
         <p key={message.id}>{message.name}</p>
       ))}
